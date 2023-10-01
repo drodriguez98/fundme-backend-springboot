eb735c9af6c9d2b038b7bd203f2d6a801a753034
Diego Rodriguez <diego25598@gmail.com>
2023-10-02 01:50:07 +0200

    Add application Main

---
+++ src/main/java/com/example/fundme/FundmeApplication.java
@@ -0,0 +1,13 @@
+package com.example.fundme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundmeApplication.class, args);
+	}
+
+}
