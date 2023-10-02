45a7f7aa3627a67fa1b6ed73a7ca90fd20c41f31
Diego Rodriguez <diego25598@gmail.com>
2023-10-02 02:23:06 +0200

    Add application Main

--- src/main/java/com/campusdual/fundme/FundmeApplication.java
+++ src/main/java/com/campusdual/fundme/FundmeApplication.java
@@ -1,4 +1,4 @@
-package com.example.fundme;
+package com.campusdual.fundme;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
