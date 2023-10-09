a266c7ed32fd903a4ba1be48e01c1fcda0b8d7c2
Diego Rodriguez <diego25598@gmail.com>
2023-10-09 19:30:39 +0200

    Add main class with jdbc session notation

--- src/main/java/com/campusdual/fundme/FundmeApplication.java
+++ src/main/java/com/campusdual/fundme/FundmeApplication.java
@@ -2,12 +2,16 @@ package com.campusdual.fundme;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
 
 @SpringBootApplication
+@EnableJdbcHttpSession
 public class FundmeApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(FundmeApplication.class, args);
+
 	}
 
 }
