3d2c5d19645e425f49c813396daa9ed233222452
Diego Rodriguez <diego25598@gmail.com>
2023-10-13 01:30:04 +0200

    Add main application

--- src/main/java/com/campusdual/fundme/FundmeApplication.java
+++ src/main/java/com/campusdual/fundme/FundmeApplication.java
@@ -2,10 +2,8 @@ package com.campusdual.fundme;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
 
 @SpringBootApplication
-@EnableJdbcHttpSession
 public class FundmeApplication {
 
 	public static void main(String[] args) {
