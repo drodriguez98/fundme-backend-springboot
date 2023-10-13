62975e4b702492c7fecec3cb1649433b768e89d8
Diego Rodriguez <diego25598@gmail.com>
2023-10-13 02:31:32 +0200

    Add UserDAO interface for User entity using JpaRepository

--- src/main/java/com/campusdual/fundme/model/dao/UserDAO.java
+++ src/main/java/com/campusdual/fundme/model/dao/UserDAO.java
@@ -3,4 +3,8 @@ package com.campusdual.fundme.model.dao;
 import com.campusdual.fundme.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserDAO extends JpaRepository<User, Integer> {}
+public interface UserDAO extends JpaRepository<User, Integer> {
+
+    User findByUsername(String username);
+
+}
