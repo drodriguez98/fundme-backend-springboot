6499d09e47f937cbd749c82f6b016bd08c21ff33
Diego Rodriguez <diego25598@gmail.com>
2023-10-13 17:50:04 +0200

    Add CustomUserDetailsService with comments

--- src/main/java/com/campusdual/fundme/service/CustomUserDetailsService.java
+++ src/main/java/com/campusdual/fundme/service/CustomUserDetailsService.java
@@ -1,5 +1,9 @@
 package com.campusdual.fundme.service;
 
+// Esta clase implementa la interfaz UserDetailsService de Spring Security.
+
+// Su función es cargar los detalles del usuario desde la base de datos, como el nombre de usuario y la contraseña encriptada, y devolverlos como un objeto UserDetails.
+
 import com.campusdual.fundme.model.User;
 import com.campusdual.fundme.model.dao.UserDAO;
 import org.springframework.beans.factory.annotation.Autowired;
