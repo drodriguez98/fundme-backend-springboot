7647c1c4de9c3d36f59917f602c57b12c34d581b
Diego Rodriguez <diego25598@gmail.com>
2023-10-13 05:48:29 +0200

    Add config package and Spring Security configuration file

--- src/main/java/com/campusdual/fundme/config/SecurityConfig.java
+++ src/main/java/com/campusdual/fundme/config/SecurityConfig.java
@@ -6,10 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
-// Permite acceso sin autenticación a rutas que comienzan con /api/public/ utilizando el método de autenticación básico
 
 @Configuration
 @EnableWebSecurity
