e2acd4d6e6cd25138d5e253260ccc3cf753752cd
Diego Rodriguez <diego25598@gmail.com>
2023-10-14 21:18:27 +0200

    Add application repositories (DAO) with comments

--- src/main/java/com/campusdual/fundme/model/dao/UserDAO.java
+++ src/main/java/com/campusdual/fundme/model/dao/UserDAO.java
@@ -1,7 +1,6 @@
 package com.campusdual.fundme.model.dao;
 
 // Este archivo define una interfaz que extiende JpaRepository, que proporciona métodos para realizar operaciones de base de datos en la entidad User.
-
 // Contiene una declaración de método para buscar usuarios por nombre de usuario.
 
 import com.campusdual.fundme.model.User;
