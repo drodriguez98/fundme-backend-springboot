591efc54f4442ba1ba88a79ef49e2544882888eb
Diego Rodriguez <diego25598@gmail.com>
2023-10-15 19:29:57 +0200

    Add application web controller and HTML templates files

--- src/main/java/com/campusdual/fundme/webcontroller/WebController.java
+++ src/main/java/com/campusdual/fundme/webcontroller/WebController.java
@@ -24,6 +24,33 @@ public class WebController {
     public String loginPage() { return "login"; }
 
     @GetMapping(value = "/dashboard")
-    public String welcomeToDashboard() { return "dashboard"; }
+    public String dashboard() { return "dashboard"; }
+
+    @GetMapping(value = "/createProject")
+    public String creatProject() { return "create-project"; }
+
+    @GetMapping(value = "/donations")
+    public String donations() { return "donations"; }
+
+    @GetMapping(value = "/myDonations")
+    public String myDonations() { return "my-donations"; }
+
+    @GetMapping(value = "/userProfile")
+    public String userProfile() { return "user-profile"; }
+
+    @GetMapping(value = "/viewProject")
+    public String viewProject() { return "view-project"; }
+
+    @GetMapping(value = "/myProjects")
+    public String myProjects() { return "my-projects"; }
+
+    @GetMapping(value = "/allProjects")
+    public String allProjects() { return "all-projects"; }
+
+    @GetMapping(value = "/settings")
+    public String settings() { return "settings"; }
+
+    @GetMapping(value = "/logout")
+    public String logout() { return "logout"; }
 
 }
