7ad773cfbed4e6e3facaec795047dd1f6b5b9904
Diego Rodriguez <diego25598@gmail.com>
2023-10-21 01:28:36 +0200

    Add user type check in CustomUserDetailsService

--- src/main/java/com/campusdual/fundme/service/CustomUserDetailsService.java
+++ src/main/java/com/campusdual/fundme/service/CustomUserDetailsService.java
@@ -6,10 +6,16 @@ package com.campusdual.fundme.service;
 import com.campusdual.fundme.model.User;
 import com.campusdual.fundme.model.dao.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static java.util.Collections.emptyList;
 
 @Service
@@ -18,16 +24,26 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Autowired
     private UserRepository userRepository;
 
-    // Devuelve un objeto UserDetails con los detalles del usuario (nombre de usuario y contraseña encriptada)
-
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
         User user = userRepository.findByUsername(username);
 
-        if (user == null) { throw new UsernameNotFoundException("Usuario no encontrado"); }
+        if (user == null) { throw new UsernameNotFoundException("User not found"); }
 
-        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), emptyList());
+        List<GrantedAuthority> authorities = new ArrayList<>();
+
+        if (user.isAdmin()) {
+
+            authorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+
+        } else {
+
+            authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+
+        }
+
+        return new org.springframework.security.core.userdetails.User (user.getUsername(), user.getPassword(), authorities);
 
     }
 
