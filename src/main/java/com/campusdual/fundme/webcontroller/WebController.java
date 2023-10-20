6b55db2d44dcadd38537cd4f9135fe6971577c6f
Diego Rodriguez <diego25598@gmail.com>
2023-10-21 00:11:31 +0200

    Add top and last donations models to donations page

--- src/main/java/com/campusdual/fundme/webcontroller/WebController.java
+++ src/main/java/com/campusdual/fundme/webcontroller/WebController.java
@@ -24,7 +24,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
-
 import java.util.Date;
 import java.util.List;
 
