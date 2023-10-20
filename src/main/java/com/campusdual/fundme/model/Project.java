3f87622cee3029f335df1e5721be254fb9f1fec1
Diego Rodriguez <diego25598@gmail.com>
2023-10-20 22:31:09 +0200

    Rename date_added and total_amount variables to map them

--- src/main/java/com/campusdual/fundme/model/Project.java
+++ src/main/java/com/campusdual/fundme/model/Project.java
@@ -18,15 +18,15 @@ public class Project {
     private User user_id;
     @Column
     private String title;
-    @Column
-    private Date date_added;
+    @Column(name = "date_added")
+    private Date dateAdded;
     @ManyToOne
     @JoinColumn(name = "area_id")
     private Area area_id;
     @Column
     private String description;
-    @Column
-    private int total_amount;
+    @Column(name = "total_amount")
+    private int totalAmount;
 
     public int getProject_id() { return project_id; }
     public void setProject_id(int project_id) { this.project_id = project_id; }
@@ -34,13 +34,13 @@ public class Project {
     public void setUser_id(User user_id) { this.user_id = user_id; }
     public String getTitle() { return title; }
     public void setTitle(String title) { this.title = title; }
-    public Date getDate_added() { return date_added; }
-    public void setDate_added(Date date_added) { this.date_added = date_added; }
+    public Date getDateAdded() { return dateAdded; }
+    public void setDateAdded(Date dateAdded) { this.dateAdded = dateAdded; }
     public Area getArea_id() { return area_id; }
     public void setArea_id(Area area_id) { this.area_id = area_id; }
     public String getDescription() { return description; }
     public void setDescription(String description) { this.description = description; }
-    public int getTotal_amount() { return total_amount; }
-    public void setTotal_amount(int total_amount) { this.total_amount = total_amount; }
+    public int getTotalAmount() { return totalAmount; }
+    public void setTotalAmount(int totalAmount) { this.totalAmount = totalAmount; }
 
 }
