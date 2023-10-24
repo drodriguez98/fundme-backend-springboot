e5e765a22dfbc127457b984c23003fc34c383fe5
Diego Rodriguez <diego25598@gmail.com>
2023-10-24 20:19:26 +0200

    Delete unused imports

--- src/main/java/com/campusdual/fundme/model/repository/CommentRepository.java
+++ src/main/java/com/campusdual/fundme/model/repository/CommentRepository.java
@@ -1,6 +1,9 @@
 package com.campusdual.fundme.model.repository;
 
 import com.campusdual.fundme.model.Comment;
+import com.campusdual.fundme.model.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Integer> {}
