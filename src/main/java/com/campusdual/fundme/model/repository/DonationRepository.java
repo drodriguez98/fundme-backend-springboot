b7444db5453474b3cffd1af86e18935480b8b1d4
Diego Rodriguez <diego25598@gmail.com>
2023-10-24 23:29:24 +0200

    Add findByUserId method in donation and project repositories

--- src/main/java/com/campusdual/fundme/model/repository/DonationRepository.java
+++ src/main/java/com/campusdual/fundme/model/repository/DonationRepository.java
@@ -1,6 +1,8 @@
 package com.campusdual.fundme.model.repository;
 
 import com.campusdual.fundme.model.Donation;
+import com.campusdual.fundme.model.Project;
+import com.campusdual.fundme.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,5 +11,6 @@ public interface DonationRepository extends JpaRepository<Donation, Integer> {
 
     List<Donation> findTop5ByOrderByAmountDesc();
     List<Donation> findTop5ByOrderByDateAddedDesc();
+    List<Donation> findByUserId(User user);
 
 }
--- src/main/java/com/campusdual/fundme/model/repository/ProjectRepository.java
+++ src/main/java/com/campusdual/fundme/model/repository/ProjectRepository.java
@@ -1,6 +1,7 @@
 package com.campusdual.fundme.model.repository;
 
 import com.campusdual.fundme.model.Project;
+import com.campusdual.fundme.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,5 +10,6 @@ public interface ProjectRepository extends JpaRepository<Project, Integer> {
 
     List<Project> findTop3ByOrderByTotalAmountDesc();
     List<Project> findTop3ByOrderByDateAddedDesc();
+    List<Project> findByUserId(User user);
 
 }
