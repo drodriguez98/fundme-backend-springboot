e403ce853ce35925552bf98a50e25529df64fe40
Diego Rodriguez <diego25598@gmail.com>
2023-10-29 17:01:49 +0100

    Add notifications jpa repository

---
+++ src/main/java/com/campusdual/fundme/model/repository/NotificationRepository.java
@@ -0,0 +1,13 @@
+package com.campusdual.fundme.model.repository;
+
+import com.campusdual.fundme.model.Notification;
+import com.campusdual.fundme.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface NotificationRepository extends JpaRepository<Notification, Integer> {
+
+    List<Notification> findByRecipientAndRead(User user, boolean read);
+
+}
