c56be6cdddf0a035787ec2c3654ed98744f4a4a9
Diego Rodriguez <diego25598@gmail.com>
2023-12-11 16:39:01 +0100

    allow options method in http requests and add filter to validate jwt

--- src/main/java/com/campusdual/fundme/config/SecurityConfig.java
+++ src/main/java/com/campusdual/fundme/config/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import org.springframework.context.annotation.Lazy;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -15,49 +17,48 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    /*
     @Autowired
     @Lazy
     private AjaxRoleFilter ajaxRoleFilter;
+    */
+
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
         http
-                .addFilterBefore(ajaxRoleFilter, UsernamePasswordAuthenticationFilter.class) // Agrega el filtro antes de la autorización
+                //.addFilterBefore(ajaxRoleFilter, UsernamePasswordAuthenticationFilter.class) // Agrega el filtro antes de la autorización
 
                 .csrf().disable()
-
                 .authorizeRequests()
-                .antMatchers("/login", "/register").permitAll()
-                .antMatchers("/**").authenticated()
+                .antMatchers("/users/authentication").permitAll()
+                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                //.antMatchers("/login", "/register").permitAll()
+                //.antMatchers("/**").authenticated()
                 //.antMatchers("/**").hasRole("ADMIN")
                 //.antMatchers("/search").authenticated() // Requiere autenticación, pero no necesita el rol AJAX_ROLE
-
                 .anyRequest().authenticated()
                 .and()
-
-                .formLogin()
-                .loginPage("/login")
-                .loginProcessingUrl("/authentication")
-                .defaultSuccessUrl("/dashboard", true)
-                .and()
-
-                .logout()
-                .logoutUrl("/logout")
-                .logoutSuccessUrl("/login")
-                .permitAll()
-                .and()
-
                 .exceptionHandling()
                 .accessDeniedPage("/error");
+
+        // Añade el filtro JWT antes del filtro UsernamePasswordAuthenticationFilter
+
+        http.addFilterBefore(new JwtTokenFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() { return new BCryptPasswordEncoder(); }
 
+    /*
     @Bean
     public AjaxRoleFilter ajaxRoleFilter() { return new AjaxRoleFilter(); }
+     */
 
 }
