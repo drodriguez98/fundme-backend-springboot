52502a1a28b70e6e592c7d23bdcf9f2d9d48d773
Diego Rodriguez <diego25598@gmail.com>
2023-12-11 16:37:20 +0100

    add schema for auth request and response

---
+++ src/main/java/com/campusdual/fundme/model/AuthResponse.java
@@ -0,0 +1,19 @@
+package com.campusdual.fundme.model;
+
+public class AuthResponse {
+    private String token;
+
+    public AuthResponse(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    // Getters y setters
+}
