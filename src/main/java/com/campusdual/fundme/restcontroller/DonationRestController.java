ebdc2b61202dcb0fcf320c5e10ce56bcce61a176
Diego Rodriguez <diego25598@gmail.com>
2023-12-13 04:58:53 +0100

    update method to get donations by user

--- src/main/java/com/campusdual/fundme/restcontroller/DonationRestController.java
+++ src/main/java/com/campusdual/fundme/restcontroller/DonationRestController.java
@@ -2,8 +2,10 @@ package com.campusdual.fundme.restcontroller;
 
 import com.campusdual.fundme.api.IDonationService;
 import com.campusdual.fundme.model.Donation;
+import com.campusdual.fundme.model.Project;
 import com.campusdual.fundme.model.dto.DonationDTO;
 import com.campusdual.fundme.model.dto.ProjectDTO;
+import com.campusdual.fundme.model.dto.UserDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
@@ -18,8 +20,11 @@ public class DonationRestController {
     @PostMapping(value = "/get")
     public DonationDTO queryDonation (@RequestBody DonationDTO donation) { return this.donationService.getDonation(donation); }
 
-    @PostMapping(value="getByProject")
+    @PostMapping(value="/getByProject")
     public List<Donation> queryDonationsByProject (@RequestBody ProjectDTO project) { return this.donationService.getDonationsByProjectId(project); }
+
+    @PostMapping(value="/getByUser")
+    public List<Donation> queryDonationsByUser (@RequestBody UserDTO user) { return this.donationService.getDonationsByUserId(user); }
 
     @GetMapping(value = "/all")
     public List<DonationDTO> queryAllDonations() { return this.donationService.getAllDonations(); }
